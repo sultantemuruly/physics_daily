9520c5d294b7f765f256fe39c6a67903bce03f65
sultantemuruly <sultantemuruly@gmail.com>
2025-04-08 20:10:50 +0500

    feat: insert users into db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const handler = NextAuth({
   providers: [
@@ -9,6 +12,29 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const existingUser = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, user.email!))
+          .then((res) => res[0]);
+
+        if (!existingUser) {
+          await db.insert(users).values({
+            name: user.name ?? "",
+            email: user.email!,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.error("Error during signIn callback:", error);
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
